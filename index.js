a8b1d9cc711a16c0ddaab99bc61f58af00e97e8d
Loïc <hello@loichorellou.net>
2019-10-21 15:44:07 +0200

    update

--- index.js
+++ index.js
@@ -6,4 +6,19 @@ const app 				= express()
 const fs 				= require('fs')
 
 
+const Logo = require('./Logo')
+
+
 console.log("des papiers ok")
+
+
+logo_entete = new Logo(200,300, "Haute école des arts du Rhin", "HEAR")
+
+
+logo_identite = new Logo(500,300, "Atelier Com Graph", "CG")
+
+
+
+// console.log( logo_entete.generateLogo() )
+// 
+logo_entete.exportLogo()
