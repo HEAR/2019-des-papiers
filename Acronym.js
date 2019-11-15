95dce57ff78a89e1be25648e7a7c232cee6a6f5e
Loïc <hello@loichorellou.net>
2019-11-15 16:05:24 +0100

    Update Acronym.js

--- Acronym.js
+++ Acronym.js
@@ -1,25 +1,25 @@
-var fs        	= require('fs');
+var fs = require('fs');
 
 class Acronym {
 
-  constructor() { 
-		this.first 				= fs.readFileSync('texts/first2.txt', 'utf8').split("\n");
-		this.second   			= fs.readFileSync('texts/second2.txt', 'utf8').split("\n");
-		this.third				= fs.readFileSync('texts/third2.txt', 'utf8').split("\n");
-		this.fourth				= fs.readFileSync('texts/fourth2.txt', 'utf8').split("\n");
+	constructor() {
+		this.first = fs.readFileSync('texts/first2.txt', 'utf8').split("\n");
+		this.second = fs.readFileSync('texts/second2.txt', 'utf8').split("\n");
+		this.third = fs.readFileSync('texts/third2.txt', 'utf8').split("\n");
+		this.fourth = fs.readFileSync('texts/fourth2.txt', 'utf8').split("\n");
 
-		this.a 						= '';
-		this.b 						= '';
-		this.c 						= '';
-		this.d 						= '';
+		this.a = '';
+		this.b = '';
+		this.c = '';
+		this.d = '';
 
-		this.stopwords 		= ['de', 'et', 'la', 'du', 'des'];
-		this.avecpoints 	= false;
+		this.stopwords = ['de', 'et', 'la', 'du', 'des'];
+		this.avecpoints = false;
 	}
 
 	choicer(somelist) {
-	  var i = Math.floor(Math.random()*somelist.length);
-	  return somelist[i];
+		var i = Math.floor(Math.random() * somelist.length);
+		return somelist[i];
 	}
 
 	/* --------- Create acronym and name ---------- */
@@ -27,14 +27,15 @@ class Acronym {
 	getacronym(wordphrase) {
 		var wplist = wordphrase.split(' ');
 
+		//console.log("wplist", wplist)
 		var acro = '';
 
-		for (var i = 0; i < wplist.length; i++) {		
-			if (!this.stopwords.includes(wplist[i])) {	
+		for (var i = 0; i < wplist.length; i++) {
+			if (!this.stopwords.includes(wplist[i])) {
 
 				var s = wplist[i].charAt(0).toUpperCase();
 
-				if (s == 'É') s = 'E'; 
+				if (s == 'É') s = 'E';
 
 				if (this.avecpoints) {
 					acro += s + '.';
@@ -48,63 +49,69 @@ class Acronym {
 	}
 
 	getAcronymAndName() {
-		var temp 						= this.choicer(this.first).split(',');
-		var temp2 					= this.choicer(this.second).split(',');
+		var temp = this.choicer(this.first).split(',');
+		var temp2 = this.choicer(this.second).split(',');
 
-		this.a 							= temp[0].trim(); 																							// FIRST WORD(S)
+		this.a = temp[0].trim(); // FIRST WORD(S)
 
-																																												// SECOND WORD(S) adj
-		if ((temp[1].trim() === "m") && (temp[2].trim() === "s")) {												  // masc sing
+		// SECOND WORD(S) adj
+		if ((temp[1].trim() === "m") && (temp[2].trim() === "s")) { // masc sing
 			this.b = temp2[0].trim();
 
-		} else if ((temp[1].trim() === "f") && (temp[2].trim() === "s")) {									// fem sing
+		} else if ((temp[1].trim() === "f") && (temp[2].trim() === "s")) { // fem sing
 			this.b = temp2[1].trim();
 
-		} else if ((temp[1].trim() === "m") && (temp[2].trim() === "p")) {									// masc plur
+		} else if ((temp[1].trim() === "m") && (temp[2].trim() === "p")) { // masc plur
 			this.b = temp2[2].trim();
 
 		} else {
-			this.b = temp2[3].trim();																													 // fem plur
+			this.b = temp2[3].trim(); // fem plur
 		}
 
-		var temp3 				= this.choicer(this.third).split(',');
-		var temp4 				= this.choicer(this.fourth).split(',');
+		var temp3 = this.choicer(this.third).split(',');
 
-		this.c 						= temp3[0].trim(); 																					      // THIRD WORD(S)
+		console.log("temp3", temp3)
 
-																																												// FOURTH WORD(S) adj
-		if ((temp3[1].trim() === "m") && (temp3[2].trim() === "s")) {												// masc sing
+		var temp4 = this.choicer(this.fourth).split(',');
+
+		this.c = temp3[0].trim(); // THIRD WORD(S)
+
+		// FOURTH WORD(S) adj
+		if ((temp3[1].trim() === "m") && (temp3[2].trim() === "s")) { // masc sing
 			this.d = temp4[0].trim();
 
-		} else if ((temp3[1].trim() === "f") && (temp3[2].trim() === "s")) {								// fem sing
+		} else if ((temp3[1].trim() === "f") && (temp3[2].trim() === "s")) { // fem sing
 			this.d = temp4[1].trim();
 
-		} else if ((temp3[1].trim() === "m") && (temp3[2].trim() === "p")) {								// masc plur
+		} else if ((temp3[1].trim() === "m") && (temp3[2].trim() === "p")) { // masc plur
 			this.d = temp4[2].trim();
 
 		} else {
-			this.d = temp4[3].trim();																													// fem plur
+			this.d = temp4[3].trim(); // fem plur
 		}
 
-		this.aacro 			= this.getacronym(this.a);
-		this.bacro 			= this.getacronym(this.b);
-		this.cacro 			= this.getacronym(this.c);
-		this.dacro 			= this.getacronym(this.d);
+		this.aacro = this.getacronym(this.a);
+		this.bacro = this.getacronym(this.b);
+		this.cacro = this.getacronym(this.c);
+		this.dacro = this.getacronym(this.d);
 
 		var b = {
-			acronym2: this.aacro + this.bacro, 
-			acronym3: this.aacro + this.bacro + this.cacro, 
-			acronym4: this.aacro + this.bacro + this.cacro + this.dacro, 
-			two: this.a + " " + this.b, 
-			three: this.a + " " + this.b + " " + this.c, 
-			four: this.a + " " + this.b + " " + this.c + " " + this.d
+			acronym2: this.aacro + this.bacro,
+			acronym3: this.aacro + this.bacro + this.cacro,
+			acronym4: this.aacro + this.bacro + this.cacro + this.dacro,
+			// two: this.a + " " + this.b,
+			// three: this.a + " " + this.b + " " + this.c,
+			// four: this.a + " " + this.b + " " + this.c + " " + this.d
+			two: [this.a, this.b],
+			three: [this.a, this.b, this.c],
+			four: [this.a, this.b, this.c, this.d]
 		};
 
 		var num = Math.floor(Math.random() * 4) + 1;
 		var a = "";
 		var n = "";
 
-		if (num == 2) {																									// print 2, 3 or 4 words or word groups
+		if (num == 2) { // print 2, 3 or 4 words or word groups
 			a = b.acronym2;
 			n = b.two;
 		} else if (num == 3) {
@@ -114,7 +121,10 @@ class Acronym {
 			a = b.acronym4;
 			n = b.four;
 		}
-		return {a: a, n: n};
+		return {
+			a: a,
+			n: n
+		};
 
 		//return b;
 	}
